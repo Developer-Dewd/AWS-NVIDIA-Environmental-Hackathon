793533d5443586c86b6230ab4c34bf2cb2ed8a63
Derrick D <derrick@Derricks-MacBook-Pro.local>
2020-08-26 01:19:43 -0400

    initial commit

---
+++ SphereOS/SphereOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SphereOS
+//
+//  Created by Derrick D. on 8/25/20.
+//  Copyright © 2020 DewdLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
